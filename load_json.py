2248975e2a79651bfb8ad4a3812e930499b0d4e2
Helge Munk Jacobsen <helgemunkjacobsen@gmail.com>
2013-04-09 11:30:00 +0200

    Updated load_json.py

--- load_json.py
+++ load_json.py
@@ -1,2 +1,8 @@
 import json
 
+file_path = 'yelp-data/yelp_training_set/'
+
+file_name = ['yelp_training_set_business.json',
+             'yelp_training_set_checkin.json',
+             'yelp_training_set_review.json',
+             'yelp_training_set_user.json']
