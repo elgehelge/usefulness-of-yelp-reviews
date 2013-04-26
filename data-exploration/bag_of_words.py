9f27063dbd2739719b8335edf17ea166cecc7734
Helge Munk Jacobsen <helgemunkjacobsen@gmail.com>
2013-04-26 17:42:17 +0200

    bag_of_words.py divided into 3 files

--- data-exploration/bag_of_words.py
+++ data-exploration/bag_of_words.py
@@ -18,96 +18,53 @@
 # - Chunks (like on frontpage: http://nltk.org/index.html)
 # - Stemming (http://nltk.googlecode.com/svn/trunk/doc/book/book.html#normalizing-text)
 
-from nltk.tokenize import word_tokenize
+
 from load_json import *
 from print_log import *
-from nltk import regexp_tokenize
-import numpy
-import re
+from tokenizer import *
+import pickle
+import json
 
 # Enable logging
 logging(True)
 
 # Load data
 business, checkin, review, user = get_data()
+most_common_words = pickle.load(open("most_common_words_count10000.pkl"))
 
-
-def custom_word_tokenization(text):
-    tokenizierRegex = r'''(?x) # set flag to allow verbose regexps
-        ([A-Z]\.)+        # abbreviations, e.g. U.S.A.
-      | \w+(-\w+)*        # words with optional internal hyphens
-      | \$?\d+(\.\d+)?%?  # currency and percentages, e.g. $12.40, 82%
-      | \!+               # !+
-      | \?+               # ?+
-      | \.+               # .+
-      | [][.,;"'?():-_`]  # these are separate tokens
-    '''
-    return regexp_tokenize(text, tokenizierRegex)
-
-
-def transform_word(word):
-        pattern_photo = re.compile(r'[.]*www\.yelp\.com\/biz\_photos[.*]')
-        pattern_repeat = re.compile(r'([\S])\1\1\1+')
-        pattern_numbers = re.compile(r'\$?\d+(\.\d+)?%?')
-        # Caps: Make lowercase unless the word is ALLCAPS
-        if (word != word.upper()):
-                word = word.lower()
-        # Numbers
-        #if (pattern_numbers.search(word) is not None):
-        #        print word
-        # Prices
-        if '$' in word:
-                word = '<PRICE>'
-        # Time
-        if 'am' or 'pm' in word:
-                word = '<TIME>'
-        # Websites: Look for "www.yelp.com/biz_photos"
-        if (pattern_photo.search(word) is not None):
-                word = 'www.yelp.com/biz_photos'
-        # Repeated letteeeers: Cut 4+ repeated letters down to 3
-        repeat_matches = pattern_repeat.finditer(word)
-        for m in repeat_matches:
-                start = m.span()[0]
-                word = word[start:start+3]
-        return word
-
-
-def create_users_matrix(user_json):
-    '''This function takes the json data for the users, and returns a numpy matrix containing the same information. This matrix is then saved to the harddrive.'''
-    num_users = len(user_json)
-
-    # votes_useful, votes_funny, votes_cool, review_count, name, average_stars, user_id, type
-    num_user_variables = 8
-    user_matrix = numpy.zeros(shape=(num_users,num_user_variables))
-
-    user_counter = 0
-    for u in user_json:
-        print u['review_count']
-        #user_matrix[user_counter] = [1 1 1 1 2 2 2 2]
-        user_counter = user_counter + 1
-
-
-
-create_users_matrix(user)
-
-# Run through all reviews and construct a dictionary of all words
-print "Constructing dictionary of all words"
-all_words = {}
-for i, r in enumerate(review):
-    if (i % 1000 == 0):
+# Run through the review list and rearrange each dictionary 
+def rearrange_to_bag_of_words():
+    print "Constructing bag-of-words dictionary of reviews"
+    for i, r in enumerate(review):
+        if (i % 1000 == 0):
             log('%i/%i - %i%%' % (i, len(review), float(i)/len(review)*100))
-    for word in custom_word_tokenization(r['text']):
-            word = transform_word(word)
-            # Put in dictionary
-            if word in all_words:
-                    all_words[word] += 1
-            else:
-                    all_words[word] = 1
+##        if (i == len(review)/10):
+##            break
 
-print "Finding most common words"
-most_common_words = {}
-for i in all_words.items():
-        word = i[0]
-        count = i[1]
-        if count >= 5:
-                most_common_words[word] = count
+        # Rearrenge votes
+        r[u'votes_useful'] = r['votes']['useful']
+        r[u'votes_funny'] = r['votes']['funny']
+        r[u'votes_cool'] = r['votes']['cool']
+        del r['votes']
+
+##        text = r['text']
+##        tokens = tokenize(r['text'])
+##
+##        # New features
+##        r[u'no_of_words'] = len(tokens)
+##        #r[u'longest_word'] = max([len(word) for word in tokens])
+##        
+##        # Rearrange text into bag-of-words
+##        for word in tokens:
+##                # Put in dictionary
+##                word += '_word'
+##                if word in r:
+##                        r[word] += 1
+##                else:
+##                        r[word] = 1
+        del r['text']
+
+        # Suffix on all keys
+        for k in r.keys():
+            r[k + '_review'] = r[k]
+            del r[k]
