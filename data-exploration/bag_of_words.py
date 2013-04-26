745a81b8f379f81c21083bb09a6609325533d269
Helge Munk Jacobsen <helgemunkjacobsen@gmail.com>
2013-04-26 20:53:45 +0200

    Added review_bag_of_words10000.json which  contains the 400 most used words

--- data-exploration/bag_of_words.py
+++ data-exploration/bag_of_words.py
@@ -30,41 +30,45 @@ logging(True)
 
 # Load data
 business, checkin, review, user = get_data()
-most_common_words = pickle.load(open("most_common_words_count10000.pkl"))
+most_common_words = pickle.load(open("most_common_words_count500.pkl"))
 
 # Run through the review list and rearrange each dictionary 
-def rearrange_to_bag_of_words():
-    print "Constructing bag-of-words dictionary of reviews"
-    for i, r in enumerate(review):
-        if (i % 1000 == 0):
-            log('%i/%i - %i%%' % (i, len(review), float(i)/len(review)*100))
-##        if (i == len(review)/10):
-##            break
+log("Constructing bag-of-words dictionary of reviews")
+for i, r in enumerate(review):
+    if (i % 1000 == 0):
+        log('%i/%i - %i%%' % (i, len(review), float(i)/len(review)*100))
+    #if (i == len(review)/10):
+    #    break
 
-        # Rearrenge votes
-        r[u'votes_useful'] = r['votes']['useful']
-        r[u'votes_funny'] = r['votes']['funny']
-        r[u'votes_cool'] = r['votes']['cool']
-        del r['votes']
+    # Rearrenge votes
+    r[u'votes_useful'] = r['votes']['useful']
+    r[u'votes_funny'] = r['votes']['funny']
+    r[u'votes_cool'] = r['votes']['cool']
+    del r['votes']
 
-##        text = r['text']
-##        tokens = tokenize(r['text'])
-##
-##        # New features
-##        r[u'no_of_words'] = len(tokens)
-##        #r[u'longest_word'] = max([len(word) for word in tokens])
-##        
-##        # Rearrange text into bag-of-words
-##        for word in tokens:
-##                # Put in dictionary
-##                word += '_word'
-##                if word in r:
-##                        r[word] += 1
-##                else:
-##                        r[word] = 1
-        del r['text']
+    text = r['text']
+    tokens = tokenize(r['text'])
 
-        # Suffix on all keys
-        for k in r.keys():
-            r[k + '_review'] = r[k]
-            del r[k]
+    # New features
+    r[u'no_of_words'] = len(tokens)
+    #r[u'longest_word'] = max([len(word) for word in tokens])
+        
+    # Rearrange text into bag-of-words
+    for word in tokens:
+        # Put in dictionary
+        word += '_word'
+        if word in r:
+            r[word] += 1
+        else:
+            r[word] = 1
+    del r['text']
+
+    # Suffix on all keys
+    for k in r.keys():
+        r[k + '_review'] = r[k]
+        del r[k]
+
+size_of_data = sum([len(r) for r in review])
+log("Saving json - Data size: %i" % size_of_data)
+json.dump(review, open('review_bag_of_words.json','wb'))
+log("json saved")
